eec6e6e471f51e15ddb80801945e0670d323e70d
John Hergenroeder <jdherg@gmail.com>
2014-10-25 01:02:25 -0500

    Add more stack operator tests.

--- test.py
+++ test.py
@@ -17,6 +17,68 @@ class TestStack(unittest.TestCase):
     def test_pop2(self):
         self.assertEqual((5, 4), self.stack.pop2())
 
+    def test_add(self):
+        self.stack.add()
+        self.assertEqual([1, 2, 3, 9], self.stack)
+
+        self.stack.add()
+        self.stack.add()
+        self.stack.add()
+        self.assertEqual([15], self.stack)
+
+        self.stack.add()
+        self.assertEqual([15], self.stack)
+
+    def test_subtract(self):
+        self.stack.subtract()
+        self.assertEqual([1, 2, 3, -1], self.stack)
+
+        self.stack.subtract()
+        self.stack.subtract()
+        self.stack.subtract()
+        self.assertEqual([3], self.stack)
+
+        self.stack.subtract()
+        self.assertEqual([3], self.stack)
+
+    def test_multiply(self):
+        self.stack.multiply()
+        self.assertEqual([1, 2, 3, 20], self.stack)
+
+        self.stack.multiply()
+        self.stack.multiply()
+        self.stack.multiply()
+        self.assertEqual([120], self.stack)
+
+        self.stack.multiply()
+        self.assertEqual([120], self.stack)
+
+    def test_divide(self):
+        self.stack.divide()
+        self.assertEqual([1, 2, 3, 0], self.stack)
+
+    def test_mod(self):
+        self.stack.mod()
+        self.assertEqual([1, 2, 3, 4], self.stack)
+
+    def test_not(self):
+        self.stack.logical_not()
+        self.assertEqual([1, 2, 3, 4, 0], self.stack)
+
+        self.stack.logical_not()
+        self.assertEqual([1, 2, 3, 4, 1], self.stack)
+
+    def test_greater(self):
+        self.stack.greater()
+        self.assertEqual([1, 2, 3, 0], self.stack)
+
+        self.stack.greater()
+        self.assertEqual([1, 2, 1], self.stack)
+
+    def test_duplicate(self):
+        self.stack.duplicate()
+        self.assertEqual([1, 2, 3, 4, 5, 5], self.stack)
+
     def test_pos_roll(self):
         self.stack._roll_helper(2, 2)
         self.assertEqual([1, 2, 3, 4, 5], self.stack)
